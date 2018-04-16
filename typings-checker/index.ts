3599c0a871a27f982c6c4a934ab4bf2865e466fe
gcanti <giulio.canti@gmail.com>
2018-04-16 11:11:25 +0200

    New Feature: statically avoid overlapping keys

---
+++ typings-checker/index.ts
@@ -0,0 +1,5 @@
+import { str } from '../src'
+
+// shouldn't type-check when using a duplicate key
+// $ExpectError Argument of type 'Match<{ a: string; }>' is not assignable to parameter of type 'Match<{ a: string; }> & Match<RowLacks<"a", { a: string; }>>'
+const m = str('a').then(str('a'))
