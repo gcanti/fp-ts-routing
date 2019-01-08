f2ae0cca2cfbda652a0b99947bb0aadb88800372
gcanti <giulio.canti@gmail.com>
2019-01-08 12:58:32 +0100

    Internal: switch to dtslint

--- typings-checker/index.ts
+++
@@ -1,5 +0,0 @@
-import { str } from '../src'
-
-// shouldn't type-check when using a duplicate key
-// $ExpectError Argument of type 'Match<{ a: string; }>' is not assignable to parameter of type 'Match<{ a: string; }> & Match<RowLacks<{ a: string; }, "a">>'
-const m = str('a').then(str('a'))
