d68e541cad5ba04e176245be9f9264a219264ce2
Stefano Magrassi <1291271+StefanoMagrassi@users.noreply.github.com>
2023-06-20 12:59:54 +0200

    fix dtslint  and remove dtslint custom script (#77)

--- dtslint/index.ts
+++ dtslint/index.ts
@@ -3,16 +3,16 @@ import * as t from 'io-ts'
 import { pipe } from 'fp-ts/lib/pipeable'
 
 // shouldn't type-check when using a duplicate key
-// $ExpectError
+// @ts-expect-error
 R.str('a').then(R.str('a'))
 pipe(
   R.str('a'),
-  // $ExpectError
+  // @ts-expect-error
   R.then(R.str('a'))
 )
 
 declare const BadQuery: t.Type<{ a: string; b: number }, { a: string } & { b: number }>
-// $ExpectError
+// @ts-expect-error
 R.query(BadQuery)
 
 const PartialQuery = t.partial({ a: t.string })
